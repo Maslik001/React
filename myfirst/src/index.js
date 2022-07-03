918c14879067cc38c32899d5c5aa447b287745ba
Maslik <Maslenkov2010@gmail.com>
2022-07-03 23:19:17 +0300

    add files

--- myfirst/src/index.js
+++ myfirst/src/index.js
@@ -4,8 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const audiImg = 'https://mediaservice.audi.com/media/live/50900/fly1400x601n1/8ysar/2021.png?wid=850';
+const bmwImg = 'https://www.bmw-kz.com/content/dam/bmw/common/all-models/m-series/m235i/2021/navigation/BMW-2-Series-gran-coupe_ModelCard.png.asset.1636441815128.png'
+const opelImg = 'https://www.pngplay.com/wp-content/uploads/2/Opel-Car-Background-PNG.png'
+const carImage = [audiImg, bmwImg, opelImg];
+const carName = ['Audi', 'BMW', 'Opel'];
+const carPower = ['360hp', '220hp', '90hp'];
+let cardNew = carName.map(function (carName, index) {
+    return <div className={'card'} key={index.toString()}>
+        <img className={'car-img'} src={carImage[index]}/>
+        <h3>{carName}</h3>
+        <span>{carPower[index]}</span>
+        <button><span>Buy Now</span></button>
+    </div>
+})
+
+let cardRender = () => {
+    return cardNew;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div><span>Hello World</span></div>
+    <div className={'title'}><h1>Hello World</h1>
+        {cardRender()}
+    </div>,
 );
 reportWebVitals();
